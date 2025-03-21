bf5f9138a9457fc96ba6812981cfe3983d93190a
chekavar-inset <chekavar.inset@gmail.com>
2025-03-22 01:26:54 +0530

    sm devices changes

--- src/components/MainBlock.jsx
+++ src/components/MainBlock.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import LeftBlock from "./LeftBlock";
 import RightBlock from "./RightBlock";
 import FirstAnim from "./FirstAnim";
+import styleMain from "./css/MainBlock.module.css";
 
 function MainBlock() {
   return (
     <>
-      <FirstAnim />
-      <LeftBlock />
-      <RightBlock />
+      <div className={`${styleMain.mainBlock}`}>
+        <FirstAnim />
+        <LeftBlock />
+        <RightBlock />
+      </div>
     </>
   );
 }
